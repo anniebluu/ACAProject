7435d1f2b0f29144efe0a576c1a42d8b770de1bd
anniebluu <anniebluu@protonmail.ch>
2022-08-02 16:24:28 -0700

    slack app commit

---
+++ slackapp.py
@@ -0,0 +1,25 @@
+import logging
+import os
+
+from dotenv import load_dotenv
+from slack_bolt import App
+from slack_bolt.adapter.socket_mode import SocketModeHandler
+
+logging.basicConfig(level=logging.INFO)
+load_dotenv()
+
+SLACK_BOT_TOKEN = os.environ["SLACK_BOT_TOKEN"]
+SLACK_APP_TOKEN = os.environ["SLACK_APP_TOKEN"]
+app = App(token=SLACK_BOT_TOKEN)
+
+@app.event("app_mention")
+def mention_handler(body, context, payload, options, say, event):
+    say("Hello World!")
+
+@app.event("message")
+def message_handler(body, context, payload, options, say, event):
+    pass
+
+if __name__ == "__main__":
+    handler = SocketModeHandler(app, SLACK_APP_TOKEN)
+    handler.start()
